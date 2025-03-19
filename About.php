3dff2d3a1fae70ec0c17ce43bf5987ea028203f3
k4350 <kaushik4350@gmail.com>
2025-03-19 21:26:54 +0530

    all files added

---
+++ About.php
@@ -0,0 +1,200 @@
+ 
+
+<section id="content">
+	<section class="section-padding">
+		<div class="container">
+			<div class="row showcase-section">
+				<div class="col-md-6">
+					<img src="img/about.png" alt="showcase image">
+				</div>
+				<div class="col-md-6">
+					<div class="about-text">
+						<p>At JobRISE,we believe in the power of personalization. Job hunting can be a daunting task, and we understand that finding the right job can be challenging. That's why we offer a highly customized experience for job seekers, catering to their unique requirements and preferences. Whether it's the desire to learn new skills, work in an inclusive environment, or have a fast-track career, we have got you covered. Our advanced technology ensures that your job search results are tailored to your specific needs, making your job search journey more comfortable and efficient.</p>
+						 <p>JobRISE aims to be more than just a job portal. We are committed to our users' entire career growth, not just finding their next job. Our decades of industry insights and cutting-edge technology come together to offer a holistic experience that empowers job seekers to explore different possibilities and take control of their career. We connect jobseekers and recruiters through a highly personalized approach, matching candidate profiles to relevant job openings. At JobRISE, we believe that everyone deserves the opportunity to find their dream job, and we are here to make that happen.</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<div class="container">
+					
+					<div class="about">
+				
+						
+						<div class="row">
+							<div class="col-md-4">
+								<!-- Heading and para -->
+								<div class="block-heading-two">
+									<h3><span>Why Choose Us?</span></h3>
+								</div>
+								<p>JobRISE is one of those job portals which are user-friendly, cost-effective, and demands a lot of attention in today's industrial and commercial world. The job search and recruitment process usually is tedious but with our website, it has become more convenient and hassle-free to register as a recruiter or a job seeker on our website. Our website has a feature where you can upload your resume on the site and get multiple jobs with a single click.</p>
+							</div>
+							<div class="col-md-4">
+								<div class="block-heading-two">
+									<h3><span>Our Solution</span></h3>
+								</div>		
+								<!-- Accordion starts -->
+								<div class="panel-group" id="accordion-alt3">
+								 <!-- Panel. Use "panel-XXX" class for different colors. Replace "XXX" with color. -->
+								  <div class="panel">	
+									<!-- Panel heading -->
+									 <div class="panel-heading">
+										<h4 class="panel-title">
+										  <a data-toggle="collapse" data-parent="#accordion-alt3" href="#collapseOne-alt3">
+											<i class="fa fa-angle-right"></i> Accordion Heading Text Item # 1
+										  </a>
+										</h4>
+									 </div>
+									 <div id="collapseOne-alt3" class="panel-collapse collapse">
+										<!-- Panel body -->
+										<div class="panel-body">
+										  Sed ut perspiciaatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas
+										</div>
+									 </div>
+								  </div>
+								  <div class="panel">
+									 <div class="panel-heading">
+										<h4 class="panel-title">
+										  <a data-toggle="collapse" data-parent="#accordion-alt3" href="#collapseTwo-alt3">
+											<i class="fa fa-angle-right"></i> Accordion Heading Text Item # 2
+										  </a>
+										</h4>
+									 </div>
+									 <div id="collapseTwo-alt3" class="panel-collapse collapse">
+										<div class="panel-body">
+										  Sed ut perspiciaatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas
+										</div>
+									 </div>
+								  </div>
+								  <div class="panel">
+									 <div class="panel-heading">
+										<h4 class="panel-title">
+										  <a data-toggle="collapse" data-parent="#accordion-alt3" href="#collapseThree-alt3">
+											<i class="fa fa-angle-right"></i> Accordion Heading Text Item # 3
+										  </a>
+										</h4>
+									 </div>
+									 <div id="collapseThree-alt3" class="panel-collapse collapse">
+										<div class="panel-body">
+										  Sed ut perspiciaatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas
+										</div>
+									 </div>
+								  </div>
+								  <div class="panel">
+									 <div class="panel-heading">
+										<h4 class="panel-title">
+										  <a data-toggle="collapse" data-parent="#accordion-alt3" href="#collapseFour-alt3">
+											<i class="fa fa-angle-right"></i> Accordion Heading Text Item # 4
+										  </a>
+										</h4>
+									 </div>
+									 <div id="collapseFour-alt3" class="panel-collapse collapse">
+										<div class="panel-body">
+										  Sed ut perspiciaatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas
+										</div>
+									 </div>
+								  </div>
+								</div>
+								<!-- Accordion ends -->
+								
+							</div>
+							
+							<div class="col-md-4">
+								<div class="block-heading-two">
+									<h3><span>Our Expertise</span></h3>
+								</div>								
+								<h6>Web Development</h6>
+								<div class="progress pb-sm">
+								  <!-- White color (progress-bar-white) -->
+								  <div class="progress-bar progress-bar-red" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: 40%">
+									 <span class="sr-only">40% Complete (success)</span>
+								  </div>
+								</div>
+								<h6>Designing</h6>
+								<div class="progress pb-sm">
+								  <div class="progress-bar progress-bar-green" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%">
+									 <span class="sr-only">40% Complete (success)</span>
+								  </div>
+								</div>
+								<h6>User Experience</h6>
+								<div class="progress pb-sm">
+								  <div class="progress-bar progress-bar-lblue" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100" style="width: 80%">
+									 <span class="sr-only">40% Complete (success)</span>
+								  </div>
+								</div>
+								<h6>Development</h6>
+								<div class="progress pb-sm">
+								  <div class="progress-bar progress-bar-yellow" role="progressbar" aria-valuenow="30" aria-valuemin="0" aria-valuemax="100" style="width: 30%">
+									 <span class="sr-only">40% Complete (success)</span>
+								  </div>
+								</div>
+							</div>
+							
+						</div>
+						
+						 						
+						 
+						<br>
+						<!-- Our Team starts -->
+				
+						<!-- Heading -->
+						<div class="block-heading-six">
+							<h4 class="bg-color">Our Team</h4>
+						</div>
+						<br>
+						
+						<!-- Our team starts -->
+						
+						<div class="team-six">
+							<div class="row">
+								<div class="col-md-3 col-sm-6">
+									<!-- Team Member -->
+									<div class="team-member">
+										<!-- Image -->
+										<img class="img-responsive" src="img/kaushik.png" alt="">
+										<!-- Name -->
+										<h4>Kaushik Baruah</h4>
+										<span class="deg">CEO</span> 
+									</div>
+								</div>
+								<div class="col-md-3 col-sm-6">
+									<!-- Team Member -->
+									<div class="team-member">
+										<!-- Image -->
+										<img class="img-responsive" src="img/bonti.png" alt="">
+										<!-- Name -->
+										<h4>Bonti Baruah</h4>
+										<span class="deg">COO</span> 
+									</div>
+								</div>
+								<div class="col-md-3 col-sm-6">
+									<!-- Team Member -->
+									<div class="team-member">
+										<!-- Image -->
+										<img class="img-responsive" src="img/srishti.png" alt="">
+										<!-- Name -->
+										<h4>Srishti Agarwal</h4>
+										<span class="deg">CFO</span> 
+									</div>
+								</div>
+								<div class="col-md-3 col-sm-6">
+									<!-- Team Member -->
+									<div class="team-member">
+										<!-- Image -->
+										<img class="img-responsive" src="img/prasurjya.png" alt="">
+										<!-- Name -->
+										<h4>Prasurjya Sarmah</h4>
+										<span class="deg">CMO</span> 
+									</div>
+								</div>
+							</div>
+						</div>
+						
+						<!-- Our team ends -->
+					  
+						
+					</div>
+									
+				</div>
+	</section> 
+ 
